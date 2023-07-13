98aa7e16b4a11ad9db56a16410170a5e310c7f69
LivaniaViegas <livaniagarreth@gmail.com>
2023-07-13 05:06:56 +0100

    SeedDb

--- SuperShop/Data/Entities/Product.cs
+++ SuperShop/Data/Entities/Product.cs
@@ -8,11 +8,9 @@ namespace SuperShop.Data.Entities
     {
         public int Id{ get; set; }
 
+        [Required]
+        [MaxLength(50, ErrorMessage = "The field {0} can contain {1} characters lenght.")]
         public string Name { get; set; }
-
-        //[Required]
-        //[MaxLength(50, ErrorMessage = "The field {0} can contain {1} characters lenght.")]
-        //public string Name { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
         public decimal Price { get; set; }
